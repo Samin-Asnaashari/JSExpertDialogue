2be1f11e58c7397a09eb4c02905cc102a6d26535
Samin.asnaashari <samin.asnaashari@accenture.com>
2019-07-23 06:37:25 +0200

    Initial + Globall installations

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+/**
+ * Install:
+ * npm i prop-types
+ * npm install @material-ui/core
+ * npm i react-router-dom
+ * npm i react-router-native ??
+ * npm i axios
+ * npm install node-sass
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
